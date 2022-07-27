ef841ec20628c416ee452af32de706f0cd74906d
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2022-07-27 20:25:51 -0300

    feat: 404 layout component translated

--- components/layout/404.js
+++ components/layout/404.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { css } from '@emotion/react';
+import React from "react";
+import { css } from "@emotion/react";
 
 const Error404 = () => {
-    return ( 
-        <h1
-            css={css`
-                margin-top: 5rem;
-                text-align: center;
-            `}
-        >No se puede mostrar</h1> 
-    );
-}
- 
+  return (
+    <h1
+      css={css`
+        margin-top: 5rem;
+        text-align: center;
+      `}
+    >
+      This page could not be found
+    </h1>
+  );
+};
+
 export default Error404;
